19a2a2501dcd282852e1a3b5d785f9e511d33e05
yellowbigbird@gmail.com <yellowbigbird@gmail.com@ea13c67a-eaad-0200-0eab-30ca1f3458a7>
2013-06-28 09:35:27 +0000

    add 7zip

---
+++ branches/testlzma/TestLzmaLib/lzmaWrapper.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+
+class CLzmaWrapper{
+public:
+    CLzmaWrapper();
+    virtual ~CLzmaWrapper();
+
+    bool Encode(const std::string& strSrc, std::string& strOut);
+    bool Decode(const std::string& strSrc, std::string& strOut);
+};
