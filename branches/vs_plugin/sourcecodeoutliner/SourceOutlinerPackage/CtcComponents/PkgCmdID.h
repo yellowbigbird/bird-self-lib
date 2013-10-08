9ffd0fa308efa2d68ed961b8394f4ed6c25a4b1c
yellowbigbird@gmail.com <yellowbigbird@gmail.com@ea13c67a-eaad-0200-0eab-30ca1f3458a7>
2013-10-08 08:10:03 +0000

    add SourceOutline

---
+++ branches/vs_plugin/sourcecodeoutliner/SourceOutlinerPackage/CtcComponents/PkgCmdID.h
@@ -0,0 +1,41 @@
+﻿/***************************************************************************
+
+Copyright (c) Microsoft Corporation. All rights reserved.
+This code is licensed under the Visual Studio SDK license terms.
+THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+
+***************************************************************************/
+
+// Contains a list of Command IDs used in defining command bars.
+// Native equivalent of PkgCmdID.cs.
+
+// Do not use #pragma once - used by the CTC compiler.
+#ifndef __PKGCMDID_H_
+#define __PKGCMDID_H_
+
+///////////////////////////////////////////////////////////////////////////////
+// Menu IDs
+
+///////////////////////////////////////////////////////////////////////////////
+// Menu Group IDs
+
+#define MyMenuGroup                 0x1020
+#define PowerToysSubMenuGroup 0x1150
+
+///////////////////////////////////////////////////////////////////////////////
+// Command IDs
+
+#define cmdidSourceOutliner1 0x102
+
+///////////////////////////////////////////////////////////////////////////////
+// Bitmap IDs
+#define bmpPic1 1
+#define bmpPic2 2
+#define bmpPicSmile 3
+#define bmpPicX 4
+#define bmpPicArrows 5
+
+#endif // __PKGCMDID_H_
---
+++ branches/vs_plugin/sourcecodeoutliner/SourceOutlinerPackage/PkgCmdID.cs
@@ -0,0 +1,22 @@
+﻿/***************************************************************************
+
+Copyright (c) 2006 Microsoft Corporation. All rights reserved.
+
+***************************************************************************/
+
+// Contains a list of Command IDs used in defining command bars.
+// This file MUST match PkgCmdID.h (for the satellite DLL).
+
+using System;
+
+namespace Microsoft.Samples.VisualStudio.PowerToy.SourceOutliner
+{
+    /// <summary>
+    /// Contains a list of all Command IDs for the package.
+    /// </summary>
+    /// <remarks>The IDs in this class must match the definitions in the file PkgCmdID.h.</remarks>
+    static class PkgCmdIDList
+    {
+        public const uint cmdidSourceOutliner1 = 0x102;
+    };
+}
