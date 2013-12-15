f424adbc44e9430dd6b13cadd6ee50ece5fced92
yellowbigbird <yellowbigbird@gmail.com>
2013-12-15 09:49:18 +0000

    rule ok

--- branches/FreeCellCalculate/FreeCellCalculateC++/Calculate.h
+++ branches/FreeCellCalculate/FreeCellCalculateC++/Calculate.h
@@ -1,21 +1,35 @@
 #pragma once
 
 #include "card.h"
+using namespace Card;
 
 class CCalculate
 {
+    typedef std::vector<int>    VecIdx;
 public:
     CCalculate();
 
     void InitData();
+    void InputData();
     void Run();
 
+    bool CheckInputDataLegal() const;
+    bool FWin() const;
+    bool FSorted(const VecIdx& vecIdx) const;
+    bool FCanMove() const;
+    bool FCanMove(int colIdx) const;
+    bool FCanMoveToSorted(const VecIdx& vecIdx) const;
+    bool FCanPushToEnd(const VecIdx& vecIdx, int cardIdx) const;
+    bool IsBenchHaveBlank() const;  
+    int  GetCurMoveCardAmount() const;
+    void GetLastSortedList(const VecIdx& vecIdx,  int moveMax, VecIdx& vecIdxSorted) const;
+
 public:
-    typedef std::vector<int>    VecIdx;
 
     std::vector<VecIdx>         m_vecVecIdx;
-    std::vector<VecIdx>         m_vecVecIdxSorted;   //4 vec
+    VecIdx                      m_vecIdxSorted;   //4 vec,save show card
     VecIdx                      m_vecBench;       //4 card  
 
-    VecCard             m_vecCardAll;   //4*13
+    //VecCard             m_vecCardAll;   //4*13
 };
+
