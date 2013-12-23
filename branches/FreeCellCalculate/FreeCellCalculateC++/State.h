87c18643ef0bbcb2086fef853b614cbb88d73586
yellowbigbird@gmail.com <yellowbigbird@gmail.com@ea13c67a-eaad-0200-0eab-30ca1f3458a7>
2013-12-23 15:26:09 +0000

    change vector card to list

--- branches/FreeCellCalculate/FreeCellCalculateC++/State.h
+++ branches/FreeCellCalculate/FreeCellCalculateC++/State.h
@@ -10,7 +10,7 @@ class CState
 {
 public:
     CState();
-    CState(const CState& other);
+    //CState(const CState& other);
     
     void InitData();
     void InputData();
@@ -23,32 +23,35 @@ public:
     bool operator == (const CState& other) const;
     bool operator < (const CState& other) const;
 
-    void GenerateSonState(std::vector<CState>& vecState);
+    void GenerateSonState(std::list<CState>& vecState);
     
-    bool CheckInputDataLegal() const;
+    //bool CheckInputDataLegal() const;
     bool FWin() const;
-    bool FSorted(const VecCard& vecIdx) const;
+    bool FSorted(const ListCard& vecIdx) const;
     bool FCanMove() const;
     
-    bool IsBenchHaveBlank() const;  
+    //bool IsBenchHaveBlank() const;  
     int  GetCurMoveCardAmount() const;
-    void GetLastSortedList(const VecCard& vecCard,  int moveMax, VecCard& vecIdxSorted) const;
+    void GetLastSortedList(const ListCard& vecCard,  int moveMax, ListCard& vecIdxSorted) const;
 
 protected:
     double UpdateValue();
     void UpdateString();
     
-    bool MoveColToBench     (std::vector<CState>& vecState, UINT colIdx);
-    bool MoveColToSorted    (std::vector<CState>& vecState, UINT colIdx);
-    bool MoveColToCol       (std::vector<CState>& vecState, UINT colIdxSrc, UINT colIdxDest, const VecCard& vecLastSorted);
-    bool MoveBenchToSorted  (std::vector<CState>& vecState, UINT benchIdx);
-    bool MoveBenchToCol     (std::vector<CState>& vecState, UINT benchIdx, UINT colIdx);
+    bool MoveColToBench     (std::list<CState>& vecState, UINT colIdx);
+    bool MoveColToSorted    (std::list<CState>& vecState, UINT colIdx);
+    bool MoveColToCol       (std::list<CState>& vecState, UINT colIdxSrc, UINT colIdxDest, const ListCard& vecLastSorted);
+    bool MoveBenchToSorted  (std::list<CState>& vecState, UINT benchIdx);
+    bool MoveBenchToCol     (std::list<CState>& vecState, UINT benchIdx, UINT colIdx);
 
     void SetIdxFather(CState& stSon);
 
-    bool FCanMove(int colIdx) const;
-    bool FCanMoveToSorted(const VecCard& vecIdx) const;
-    bool FCanPushToEnd(const VecCard& vecIdx, CCard cardIdx) const;
+    bool FCanMove           (int colIdx) const;
+    bool FCanMoveToSorted   (const ListCard& vecIdx) const;
+    bool FCanMoveToBench    (UINT& benchIdx) const ;
+    bool FCanPushToEnd      (const ListCard& vecCards, const CCard& cardIdx) const;
+
+    UINT GetBenchEmptyCount () const;
 
 public:    
     UINT                    m_id;
@@ -60,8 +63,8 @@ public:
 
     VecCard                 m_vecIdxSorted;   //4 vec, save show card
     VecCard                 m_vecBench;       //4 card 
-    std::vector<VecCard>    m_vecVecIdx;
+    std::vector<ListCard>   m_vecVecIdx;
 };
 
-typedef std::vector<CState>     VecState;
-
+//typedef std::vector<CState>     VecState;
+typedef std::list<CState>     ListState;
