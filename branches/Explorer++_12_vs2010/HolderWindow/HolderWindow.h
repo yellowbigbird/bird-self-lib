8247890de5c688e1933940b170d68700eb4ccb5e
yellowbigbird@gmail.com <yellowbigbird@gmail.com@ea13c67a-eaad-0200-0eab-30ca1f3458a7>
2014-11-03 09:31:06 +0000

    no vs2010.

--- branches/Explorer++_12_vs2010/HolderWindow/HolderWindow.h
+++
@@ -1,8 +0,0 @@
-#ifndef HOLDERWINDOW_INCLUDED
-#define HOLDERWINDOW_INCLUDED
-
-#define WM_USER_HOLDERRESIZED		WM_APP + 300
-
-HWND	CreateHolderWindow(HWND hParent,TCHAR *szWindowName,UINT uStyle);
-
-#endif
