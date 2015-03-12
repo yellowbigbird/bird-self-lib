5c6c58a628296cf615a1dc5807a421402b0f74df
yellowbigbird@gmail.com <yellowbigbird@gmail.com@ea13c67a-eaad-0200-0eab-30ca1f3458a7>
2015-03-12 09:47:36 +0000

    can open new tab with 1280 url.

--- branches/tumblr_download/tumblr.js
+++ branches/tumblr_download/tumblr.js
@@ -3,20 +3,43 @@
 // found in the LICENSE file.
 
 // A generic onclick callback function.
+		
 function onClickHandler(info, tab) {
+	console.log("info: " + JSON.stringify(info));
+	console.log("tab: " + JSON.stringify(tab));
 	
-	var newurl = "http://www.baidu.com";
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-  //chrome.tabs.query({'active': true}, openNewTab(chrome.tabs) );
-  chrome.tabs.create(newurl, openNewTab)
+	var str540 = "_540.";
+	var str400 = "_400.";
+	var str500 = "_500.";
+	var str1280 = "_1280.";
+	var oldurl = info.srcUrl;
+	var newurl = oldurl;
+	if(newurl.indexOf(str540) > -1){	
+		newurl = oldurl.replace(str540, str1280);
+	}
+	else if(newurl.indexOf(str400) > -1){	
+		newurl = oldurl.replace(str400, str1280);
+	}
+	else if(newurl.indexOf(str500) > -1){	
+		newurl = oldurl.replace(str500, str1280);
+	}
+	else{
+		//return;
+	}
+	openNewTab(tab, newurl);
 };
 
-function openNewTab(tab) {
-	console.log("tab: " + JSON.stringify(tab) );
-	//chrome.tabs.update(tabs[0].id, newurl);
-	
+function openNewTab(tab, url0) {
+	console.log("opennewtab");
+	chrome.tabs.create({url: url0 } );
+	//chrome.tabs.create({url:chrome.extension.getURL("tabs_api.html")});
+	//chrome.tabs.update(tabs[0].id, newurl);	
+};
+
+function cloneTab(tab) {
+	chrome.tabs.update({}, function(tab) {
+      chrome.tabs.duplicate(tab.id);
+  });
 };
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
@@ -24,19 +47,18 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 // Set up context menu tree at install time.
 chrome.runtime.onInstalled.addListener(function() {
 	// Create one test item for each context type.
-	var contexts = ["page","selection","link","editable","image","video",
-					"audio"];
+	var contexts = ["image"];
+	//["page","selection","link","editable","image","video", "audio"];
+		
 	for (var i = 0; i < contexts.length; i++) {
 	  var context = contexts[i];
-	  var title = "download 1280 img";
+	  var title = "get 1280 img";
 	  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
 											 "id": "context" + context});
 	  console.log("'" + context + "' item:" + id);
 	}
 
-
 	// Create a parent item and two children.
 	var parent = chrome.contextMenus.create({"title": "Test parent item"});
 	console.log("parent:" + parent );
-
 });
