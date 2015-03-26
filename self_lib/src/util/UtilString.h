901cd6fa7a7e6da93ff8c821a7c666205ed50f70
yellowbigbird <yellowbigbird@gmail.com>
2015-03-26 09:35:14 +0000

    update selflib

--- self_lib/src/util/UtilString.h
+++ self_lib/src/util/UtilString.h
@@ -4,6 +4,9 @@
 #include "selflib_dll.h"
 //#include <WTypes.h>
 
+
+typedef std::vector<std::string> VecStr;
+
 namespace UtilString{
   SELFLIB_DLL std::wstring ConvertMultiByteToWideChar(const std::string& strSrc);
   SELFLIB_DLL std::wstring ConvertMultiByteToWideCharUTF8(const std::string& strSrc);
@@ -12,4 +15,10 @@ namespace UtilString{
   SELFLIB_DLL std::string ConvertWideCharToMultiByteUTF8(const std::wstring& wstrSrc);
 
   SELFLIB_DLL bool  ConvertHexStringToValue(const std::string& str, int& value) ;
+
+
+  SELFLIB_DLL void ChangeStringLetter(std::string& str, bool fToUp);
+  SELFLIB_DLL void ChangeStringLetter(std::wstring& wstr, bool fToUp);
+
+  SELFLIB_DLL bool SplitString(const std::string& strSrc, VecStr& vecStrDest, const std::string& strMark=",");
 }
